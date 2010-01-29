cb14bb1b199ada14a4a86c5a46b44c685afa48f9
jeff <jeff@merlin.(none)>
2010-01-29 00:58:44 -0800

    Resolved merge conflict, added expire_time arg to memoize()

--- lib/memoizable.rb
+++ lib/memoizable.rb
@@ -23,6 +23,15 @@ module Memoizable
     def writeCache
       Memoizable.writeFile YAML.dump(CACHE), CACHEFILE
     end
+    #Determine if CACHE has an unexpired key
+    #key must be the CACHE key to lookup, expire_time is the number of seconds a record is considered new, or nil
+    def has key, expire_time=nil
+      if expire_time.nil?
+        CACHE.has_key? key
+      else
+        CACHE.has_key?(key) and CACHE[key]
+      end
+    end
   end
   cache=YAML.load(Memoizable.readFile(CACHEFILE).join)
   if cache.respond_to? :has_key?
@@ -31,15 +40,18 @@ module Memoizable
     CACHE={}
   end
   module ClassMethods
-    def memoize(name)
+    def memoize(name, expire_time=nil)
       original = "__original__#{name}"
       alias_method original, name
       define_method(name) do |*args|
         key= self.to_s.unpack("a*")<<name.to_s.unpack("a*")<<args
-        unless CACHE.has_key? key
-          CACHE[key] = send(original, *args)
+        unless CACHE.has_key?(key)
+          CACHE[key] = [send(original, *args), Time.now]
+          Memoizable.writeFile YAML.dump(CACHE), CACHEFILE
         end
-        CACHE[key]
+        return CACHE[key][0] unless !expire_time.nil? and CACHE[key][1] < Time.now.-(expire_time)
+        CACHE[key] = [send(original, *args), Time.now]
+        CACHE[key][0]
       end
     end
   end
